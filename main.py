8f21f4d3c45ce752d82f2a489fff42f61663606c
haimgia <giaoriana24@gmail.com>
2026-01-26 12:52:59 -0500

    cleaned all extracted terms from duplicates and extra characters

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from extractor import extract_text
+from extractor import extract_text, clean_extracted_terms
 import os
 
+EXTRACT_TERMS = False
+CLEAN_TERMS = True
+
 #PDF = "AHA_clinical_guidelines\Heart Failure Clinical Guidelines.pdf"
-FOLDER = "AHA_clinical_guidelines"
+CLINICAL_GUIDELINES_FOLDER = "AHA_clinical_guidelines"
+EXTRACTED_TERMS_FOLDER = "extracted_terms"
+COMBINED_TERMS_FILE = "combined_extracted_terms.txt"
+FINAL_TERMS_FILE = "final_terms.txt"
 
 FINISHED_FILES = ["Acute Myocarditis Clinical Guidelines.pdf",
                   "Aortic Disease Clinical Guidelines.pdf",
@@ -13,13 +19,27 @@ FINISHED_FILES = ["Acute Myocarditis Clinical Guidelines.pdf",
 
 if __name__ == "__main__":
 
-    # for file in tqdm(os.listdir(FOLDER)):
-    #     print(file)
+   
+    if EXTRACT_TERMS:
+        for file in os.listdir(CLINICAL_GUIDELINES_FOLDER):
+            if file.endswith(".pdf") and file not in FINISHED_FILES:
+                PDF = os.path.join(CLINICAL_GUIDELINES_FOLDER, file)
+                print(f"Processing file: {PDF}")
+                extract_text(PDF)
 
-    # exit()
+    if CLEAN_TERMS:
+        with open(COMBINED_TERMS_FILE, 'w', encoding='utf-8') as combined_file:
+            for file in os.listdir(EXTRACTED_TERMS_FOLDER):
+                with open(os.path.join(EXTRACTED_TERMS_FOLDER, file), 'r', encoding='utf-8') as f:
+                    content = f.read()
+                    combined_file.write(content)
+                    combined_file.write("\n\n")
 
-    for file in os.listdir(FOLDER):
-        if file.endswith(".pdf") and file not in FINISHED_FILES:
-            PDF = os.path.join(FOLDER, file)
-            print(f"Processing file: {PDF}")
-            extract_text(PDF)
+        with open(COMBINED_TERMS_FILE, "r", encoding="utf-8") as f:
+            terms = f.readlines()
+
+        cleaned_terms = clean_extracted_terms(terms)
+
+        with open(FINAL_TERMS_FILE, "w", encoding="utf-8") as f:
+            for term in cleaned_terms:
+                f.write(f"{term}\n")
